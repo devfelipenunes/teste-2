3a4c45ab81ac76b6f0ab52c659b3d54bc31107fd
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-02-14 11:25:51 -0300

    atualizei

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    require("postcss-import"),
-    require("tailwindcss")(path.resolve(__dirname, "./tailwind.config.js")),
-    require("autoprefixer"),
-  ],
+  plugins: [react()],
 });
